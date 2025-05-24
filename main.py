24f9d8a3e71ce8e79ca4f5929fc8f4dd0c7bb9b2
Lara <laracarolinemaya@gmail.com>
2025-05-24 20:50:26 -0300

    adição de filtro por mês/ano

--- main.py
+++ main.py
@@ -40,9 +40,9 @@ st.title("Tratamento de Erro de Planilha")
 st.subheader("Correção de erros mais comuns ao tratar planilhas. Para outros, é preciso verificar manualmente.")
 st.write("")
 st.markdown("""
-**AVISOS**  
+**REGRAS DE USO**  
 - Apenas arquivos CSV e XLSX são permitidos;
-- O formato aceito pelos apps costumam ser o CSV. Logo, o resultado do tratamento é um arquivo CSV;
+- O formato aceito pelos apps costuma ser o CSV. Logo, o resultado do tratamento é um arquivo CSV;
 - O nome das colunas a serem alteradas precisam ser digitados **exatamente** como está na planilha;
 - Ao escolher a opção Adicionar Caractere, por padrão, o caractere a ser adicionado será \'A\';
 - O formato UTF-8 já é aplicado automaticamente.
@@ -62,7 +62,7 @@ if upload is not None:   # Se upload não estiver vazio, transforma arquivo em d
     df = converter_data(df)
     df = converter_valor(df)
 
-operacao = st.selectbox("O que deseja fazer? ", ("Adicionar caractere no início da coluna", "Transformar coluna em texto", "Dividir planilha (arquivo grande)"))
+operacao = st.selectbox("O que deseja fazer? ", ("Adicionar caractere no início da coluna", "Transformar coluna em texto", "Dividir planilha (arquivo grande)", "Filtrar planilha por mês/ano"))
 
 if operacao == "Adicionar caractere no início da coluna":
     coluna_a_alterar = st.text_input("Insira o nome da coluna que deseja adicionar um caractere: ")
@@ -100,3 +100,37 @@ elif operacao == "Dividir planilha (arquivo grande)":
 
         st.download_button("Baixar Parte 1", metade1.to_csv(index=False).encode('utf-8'), file_name="metade1.csv", mime="text/csv")
         st.download_button("Baixar Parte 2", metade2.to_csv(index=False).encode('utf-8'), file_name="metade2.csv", mime="text/csv")
+
+elif operacao == "Filtrar planilha por mês/ano":
+    coluna_a_filtrar = st.text_input("Insira o nome da coluna que deseja realizar o filtro")
+    if df is not None:
+        if coluna_a_filtrar in df.columns:
+            try:
+                # retorna "na" caso conversão não funcione
+                df[coluna_a_filtrar] = pd.to_datetime(df[coluna_a_filtrar], format="%d/%m/%Y", errors="coerce")
+                mes_a_filtrar = st.text_input("Insira o mês a ser filtrado [1 a 12]")
+                ano_a_filtrar = st.text_input("Insira o ano a ser filtrado")
+
+                if not mes_a_filtrar or not ano_a_filtrar:
+                    st.error("Insira mês e ano.")
+
+                mes_a_filtrar = int(mes_a_filtrar)
+                ano_a_filtrar = int(ano_a_filtrar)
+
+                if mes_a_filtrar < 1 or mes_a_filtrar > 12:
+                  st.error("Mês inválido.")
+
+                else:
+                    # df apenas com as linhas que possuem o mês/ano escolhido
+                    df = df[(df[coluna_a_filtrar].dt.month == mes_a_filtrar) & (df[coluna_a_filtrar].dt.year == ano_a_filtrar)] 
+                    df[coluna_a_filtrar] = df[coluna_a_filtrar].dt.strftime('%d/%m/%Y')
+
+                    st.dataframe(df)
+
+                    st.download_button("Baixar arquivo CSV filtrado", df.to_csv(index=False).encode('utf-8'),file_name="PlanilhaFiltrada.csv", mime="text/csv")
+
+            except Exception:
+                pass
+
+        else:
+            st.error("Coluna não encontrada.")
